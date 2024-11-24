851b02be8d0fc035cf5e9be8d0a1253ef43583dd
Rheydskey <matgag@tutanota.com>
2024-11-24 13:23:21 +0100

    Split and facto

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ import sys
 import os
 import anki
 import pathlib
-import hashlib
 from aqt import mw
 from aqt.qt import *
 from aqt import gui_hooks
@@ -13,8 +12,7 @@ sys.path.insert(0, str(pathlib.Path(os.path.dirname(__file__)) / "libs"))
 from .gen_md import DeckGenerator
 from .diff import Diff
 from .git import Git
-from .utils import get_stripped_lines
-from .mdanki import MdAnkiMigrator
+from .migrator import migrate_old_card
 
 static_html = """
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" integrity="sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+" crossorigin="anonymous">
@@ -26,101 +24,6 @@ col = mw.col
 ext_pwd = pathlib.Path(os.path.dirname(__file__))
 card_folder = ext_pwd / "cards/"
 git_repo = "https://git.rheydskey.org/rheydskey/anki-md"
-
-
-def get_from_title(note_title, cards: [(str, str, any)]) -> int | None:
-    try:
-        return list(map(lambda v: "\n".join(v[0]).strip(), cards)).index(
-            note_title.strip()
-        )
-    except:
-        return None
-
-
-def convert(noteid, card, model):
-    model = mw.col.models.by_name("Ankill")
-    note = mw.col.get_note(noteid)
-
-    old_notetype_id = note.note_type()["id"]
-    new_notetype_id = model["id"]
-
-    payload = mw.col.models.change_notetype_info(
-        old_notetype_id=old_notetype_id, new_notetype_id=new_notetype_id
-    )
-    req = payload.input
-    req.note_ids.extend([noteid])
-    mw.col.models.change_notetype_of_notes(req)
-
-    note = mw.col.get_note(noteid)
-
-    note.fields[0] = "\n".join(card[0])
-    note.fields[1] = "\n".join(card[1])
-    note.fields[2] = card[2]
-
-    mw.col.update_note(note)
-
-
-def migrate_old_card(deck: anki.decks.Deck, folder: pathlib.Path):
-    did = deck["id"]
-
-    q = f"did:{did} -note:Ankill"
-    notes = mw.col.find_notes(q)
-    migrator = MdAnkiMigrator()
-    notes_content = [
-        (
-            migrator.convert(mw.col.get_note(note).fields[0]),
-            migrator.convert(mw.col.get_note(note).fields[1]),
-        )
-        for note in notes
-    ]
-    notes_hash = [
-        hashlib.sha512(bytes(f"{r}{v}", "utf-8")).hexdigest()
-        for (r, v) in notes_content
-    ]
-
-    if len(notes) == 0:
-        return
-
-    deck_gen = DeckGenerator(deck["id"], mw)
-    cards = []
-    for entry in folder.iterdir():
-        filename, fileext = os.path.splitext(entry)
-        if entry.is_file() and fileext == ".md":
-            with open(entry, "r") as file:
-                cards.extend(
-                    list(
-                        map(
-                            lambda r: (
-                                get_stripped_lines(r[0]),
-                                get_stripped_lines(r[1]),
-                                r[2],
-                            ),
-                            deck_gen.gen_decks(file.read()),
-                        )
-                    )
-                )
-
-    card_hash = [
-        hashlib.sha512(
-            bytes(
-                f"{"\n".join(r)}{"\n".join(v)}",
-                "utf-8",
-            )
-        ).hexdigest()
-        for (r, v, _) in cards
-    ]
-
-    n = 0
-    for n, note_hash in enumerate(notes_hash):
-        if note_hash in card_hash:
-            card = cards[card_hash.index(note_hash)]
-            convert(notes[n], card, mw.col.models.by_name("Ankill"))
-        else:
-            last_try = get_from_title(notes_content[n][0], cards)
-            if last_try is not None:
-                convert(notes[n], cards[last_try], mw.col.models.by_name("Ankill"))
-            else:
-                print(f"Cannot migrate {notes[n]}")
 
 
 def init_deck(deck: anki.decks.Deck, folder: pathlib.Path):
---
+++ migrator.py
@@ -0,0 +1,91 @@
+import os
+import anki
+import pathlib
+import hashlib
+from aqt import mw
+from .gen_md import DeckGenerator
+from .utils import get_stripped_lines
+from .mdanki import MdAnkiMigrator
+
+
+def hash_card(r, v):
+    return hashlib.sha512(bytes(f"{r}{v}", "utf-8")).hexdigest()
+
+
+def get_from_title(note_title, cards: [(str, str, any)]) -> int | None:
+    try:
+        return list(map(lambda v: "\n".join(v[0]).strip(), cards)).index(
+            note_title.strip()
+        )
+    except:
+        return None
+
+
+def get_convert_content_of_note(note, migrator):
+    return (
+        migrator.convert(mw.col.get_note(note).fields[0]),
+        migrator.convert(mw.col.get_note(note).fields[1]),
+    )
+
+
+def strip_card(card):
+    return (get_stripped_lines(card[0]), get_stripped_lines(card[1]), card[2])
+
+def gen_and_strip(input, gen):
+    return list(map(strip_card, gen.gen_decks(input)))
+
+def convert(noteid, card):
+    model = mw.col.models.by_name("Ankill")
+    note = mw.col.get_note(noteid)
+
+    old_notetype_id = note.note_type()["id"]
+    new_notetype_id = model["id"]
+
+    payload = mw.col.models.change_notetype_info(
+        old_notetype_id=old_notetype_id, new_notetype_id=new_notetype_id
+    )
+    req = payload.input
+    req.note_ids.extend([noteid])
+    mw.col.models.change_notetype_of_notes(req)
+
+    note = mw.col.get_note(noteid)
+
+    note.fields[0] = "\n".join(card[0])
+    note.fields[1] = "\n".join(card[1])
+    note.fields[2] = card[2]
+
+    mw.col.update_note(note)
+
+
+def migrate_old_card(deck: anki.decks.Deck, folder: pathlib.Path):
+    did = deck["id"]
+
+    q = f"did:{did} -note:Ankill"
+    notes = mw.col.find_notes(q)
+    migrator = MdAnkiMigrator()
+    notes_content = [get_convert_content_of_note(note, migrator) for note in notes]
+    notes_hash = [hash_card(r, v) for (r, v) in notes_content]
+
+    if len(notes) == 0:
+        return
+
+    deck_gen = DeckGenerator(deck["id"], mw)
+    cards = []
+    for entry in folder.iterdir():
+        filename, fileext = os.path.splitext(entry)
+        if entry.is_file() and fileext == ".md":
+            with open(entry, "r") as file:
+                cards.extend(gen_and_strip(file.read(), deck_gen))
+
+    card_hash = [hash_card("\n".join(r), "\n".join(v)) for (r, v, _) in cards]
+
+    for n, note_hash in enumerate(notes_hash):
+        if note_hash in card_hash:
+            card = cards[card_hash.index(note_hash)]
+            convert(notes[n], card)
+        else:
+            last_try = get_from_title(notes_content[n][0], cards)
+            if last_try is not None:
+                convert(notes[n], cards[last_try])
+            else:
+                print(f"Cannot migrate {notes[n]}")
